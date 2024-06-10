8820a7c6c0862919b039d63f9c9faae6b92bf045
Luigie Bacli <luitest676@gmail.com>
2024-06-11 04:51:21 +0800

    feat: added register tab

--- src/components/LoginDialog.vue
+++ src/components/LoginDialog.vue
@@ -9,62 +9,118 @@
           <q-btn label="x" v-close-popup />
         </div>
         <div class="text-warning text-center text-h5 text-bold">
-          Login
-        </div></q-card-section
-      >
-      <div class="col q-gutter-md">
-        <q-input
-          dense
-          filled
-          label="Username / Email"
-          class="bg-warning"
-          style="border-radius: 12px"
-        />
+          <q-tabs v-model="tab"
+            ><q-tab label="Login" name="one" />
+            <q-tab label="Register" name="two"
+          /></q-tabs></div
+      ></q-card-section>
+      <q-tab-panels class="bg-primary" v-model="tab" animated>
+        <q-tab-panel name="one">
+          <div class="col q-gutter-md">
+            <q-input
+              dense
+              filled
+              label="Phone / Email"
+              class="bg-warning"
+              style="border-radius: 12px"
+            />
 
-        <q-input
-          dense
-          filled
-          label="Password"
-          class="bg-warning"
-          style="border-radius: 12px"
-        />
+            <q-input
+              dense
+              filled
+              label="Password"
+              class="bg-warning"
+              style="border-radius: 12px"
+            />
 
-        <div class="text-orange row text-subtitle4 items-center">
-          <div class="col-6">
-            <q-checkbox color="teal" size="xs" label="Remember me" />
-          </div>
-          <div class="col-6" style="cursor: pointer">Forgot Password?</div>
-        </div>
-        <div class="col-12 text-center">
-          <q-btn
-            label="Signin"
-            class="bg-warning"
-            rounded
-            style="width: 100%"
-          />
-        </div>
-        <div class="row justify-center text-orange text-bold">
-          <div
-            style="cursor: pointer; text-decoration: underline"
-            @click="showSignUpDialog = true"
-          >
-            Create An Account
-          </div>
-        </div>
-        <div class="row justify-center text-warning text-subtitle4">
-          Or login with
-        </div>
-        <div class="row justify-center">
-          <q-btn round class="bg-warning q-pa-sm"
-            ><q-avatar size="md"><img src="images/google.png" /></q-avatar
-          ></q-btn>
-        </div>
-      </div> </q-card
+            <div class="text-orange row text-subtitle4 items-center">
+              <div class="col-6">
+                <q-checkbox color="teal" size="xs" label="Remember me" />
+              </div>
+              <div class="col-6" style="cursor: pointer">Forgot Password?</div>
+            </div>
+            <div class="col-12 text-center">
+              <q-btn
+                label="Signin"
+                class="bg-warning"
+                rounded
+                style="width: 100%"
+              />
+            </div>
+            <div class="row justify-center text-orange text-bold">
+              <div
+                style="cursor: pointer; text-decoration: underline"
+                @click="showSignUpDialog = true"
+              >
+                Create An Account
+              </div>
+            </div>
+            <div class="row justify-center text-warning text-subtitle4">
+              Or login with
+            </div>
+            <div class="row justify-center">
+              <q-btn round class="bg-warning q-pa-sm"
+                ><q-avatar size="md"><img src="images/google.png" /></q-avatar
+              ></q-btn>
+            </div></div
+        ></q-tab-panel>
+        <q-tab-panel name="two"
+          ><div class="col q-gutter-md">
+            <q-input
+              dense
+              filled
+              label="Phone / Email"
+              class="bg-warning"
+              style="border-radius: 12px"
+            />
+
+            <q-input
+              dense
+              filled
+              label="Password"
+              class="bg-warning"
+              style="border-radius: 12px"
+            />
+
+            <q-input
+              dense
+              filled
+              label="Confirm Password"
+              class="bg-warning"
+              style="border-radius: 12px"
+            />
+
+            <div class="col-12 text-center q-mt-xl">
+              <q-btn
+                label="Register"
+                class="bg-warning"
+                rounded
+                style="width: 100%"
+              />
+            </div>
+            <div class="row justify-center text-orange text-bold">
+              <div
+                style="cursor: pointer; text-decoration: underline"
+                @click="showSignUpDialog = true"
+              >
+                Login
+              </div>
+            </div>
+            <div class="row justify-center text-warning text-subtitle4">
+              Or signup with
+            </div>
+            <div class="row justify-center">
+              <q-btn round class="bg-warning q-pa-sm"
+                ><q-avatar size="md"><img src="images/google.png" /></q-avatar
+              ></q-btn>
+            </div></div
+        ></q-tab-panel>
+      </q-tab-panels> </q-card
   ></q-dialog>
 </template>
 
 <script setup>
-import { computed, watch } from "vue";
+import { computed, ref, watch } from "vue";
 
 import { defineProps, defineEmits } from "vue";
 
@@ -80,6 +136,8 @@ const props = defineProps({
     type: Boolean,
   },
 });
+
+const tab = ref("one");
 
 const emit = defineEmits(["update:modelValue"]);
 
