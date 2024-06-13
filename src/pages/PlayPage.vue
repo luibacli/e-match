d60c166ed3aeaefd0fd3b896000e69c8fe3c4ef7
Luigie Bacli <luitest676@gmail.com>
2024-06-14 05:07:15 +0800

    chore: adjust padding

--- src/pages/PlayPage.vue
+++ src/pages/PlayPage.vue
@@ -25,49 +25,59 @@
     </div>
     <q-btn class="bg-warning" label="test" @click="store.getMatches" />
     <q-btn class="bg-warning" label="dota2" @click="store.getDota2" /> -->
-    <div v-if="tableLoading">Loading...</div>
-    <div v-else class="row q-pa-md q-gutter-md justify-start">
-      <q-card
-        class="bg-primary text-warning match-card"
-        v-for="game in games"
-        :key="game.id"
-        ><q-card-section class="row" horizontal
-          ><q-card-section class="text-bold">{{ game.type }}</q-card-section>
-
-          <q-card-section>ID: {{ game.id }} </q-card-section>
-        </q-card-section>
-        <q-separator />
-        <q-card-section>
-          <div class="q-pa-none bg-warning q-col-gutter-sm rounded-borders">
+    <!-- <div v-if="tableLoading">Loading...</div>
+    <div v-else class="q-mt-md">
+      <q-table
+        class="bg-primary text-warning"
+        dense
+        :columns="matchColumns"
+        :rows="matchList"
+        separator="cell"
+      ></q-table>
+    </div>
+    <q-btn class="bg-warning" label="test" @click="store.getMatches" />
+    <q-btn class="bg-warning" label="dota2" @click="store.getDota2" /> -->
+    <div v-if="tableLoading"></div>
+    <div v-else class="row q-pa-sm">
+      <div class="row q-gutter-sm justify-center">
+        <div class="row" v-for="game in games" :key="game.id">
+          <q-card class="bg-primary text-warning match-card">
             <div class="row q-pa-sm">
-              <div class="col-6">
-                <span class="text-bold text-dark">
-                  <q-icon name="person"
-                /></span>
-                <span class="text-purple"> {{ game.name }}</span>
-              </div>
-              <div class="col-6 text-dark">
-                <span class="text-dark text-bold"
-                  ><q-icon name="emoji_events"
-                /></span>
-                <span class="text-positive">10</span> /
-                <span class="text-negative">5</span>
-              </div>
+              <div class="col-12 text-subtitle2">{{ game.type }}</div>
+              <div class="col-12 text-caption">{{ game.id }}</div>
             </div>
-
-            <div class="row q-pa-sm">
-              <div class="col-12">
-                <span class="text-bold text-dark">Bet:</span>
-                <span class="text-secondary">{{ game.bet }}</span>
+            <q-separator />
+            <q-card-section>
+              <div class="q-pa-none bg-warning q-col-gutter-sm rounded-borders">
+                <div class="q-pa-sm">
+                  <div class="col-6">
+                    <span class="text-bold text-dark">
+                      <q-icon name="person" />
+                    </span>
+                    <span class="text-purple"> {{ game.host }}</span>
+                  </div>
+                  <div class="col-6 text-dark">
+                    <span class="text-dark text-bold">
+                      <q-icon name="emoji_events" />
+                    </span>
+                    <span class="text-positive">10</span> /
+                    <span class="text-negative">5</span>
+                  </div>
+                </div>
+                <div class="row q-pa-sm">
+                  <div class="col-12">
+                    <span class="text-bold text-dark">Bet:</span>
+                    <span class="text-secondary">{{ game.bet }}</span>
+                  </div>
+                </div>
               </div>
+            </q-card-section>
+            <div class="row justify-center q-mb-sm">
+              <q-btn class="bg-green" flat dense label="Challenge" size="sm" />
             </div>
-          </div>
-        </q-card-section>
-
-        <q-card-section class="row justify-center">
-          <q-btn class="bg-green" flat dense label="Challenge" />
-        </q-card-section>
-      </q-card>
+          </q-card>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -92,6 +102,7 @@ watch(matchList, (newList) => {
 <style lang="scss" scoped>
 .match-card {
   width: 100%;
-  max-width: 205px;
+  height: 100%;
+  max-width: 120px;
 }
 </style>
